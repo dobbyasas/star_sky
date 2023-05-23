79aabf8a59ec0444c92af399df312d1ff56d675e
Kryštof Svejkovský <svejkovsky.kry@gmail.com>
2023-05-23 22:31:48 +0200

    initial commit

---
+++ stars/src/App.jsx
@@ -0,0 +1,30 @@
+import './index.scss'
+
+function App() {
+  return (
+    <div className="night">
+        <div className="shooting_star"></div>
+        <div className="shooting_star"></div>
+        <div className="shooting_star"></div>
+        <div className="shooting_star"></div>
+        <div className="shooting_star"></div>
+        <div className="shooting_star"></div>
+        <div className="shooting_star"></div>
+        <div className="shooting_star"></div>
+        <div className="shooting_star"></div>
+        <div className="shooting_star"></div>
+        <div className="shooting_star"></div>
+        <div className="shooting_star"></div>
+        <div className="shooting_star"></div>
+        <div className="shooting_star"></div>
+        <div className="shooting_star"></div>
+        <div className="shooting_star"></div>
+        <div className="shooting_star"></div>
+        <div className="shooting_star"></div>
+        <div className="shooting_star"></div>
+        <div className="shooting_star"></div>
+    </div>
+  )
+}
+
+export default App
